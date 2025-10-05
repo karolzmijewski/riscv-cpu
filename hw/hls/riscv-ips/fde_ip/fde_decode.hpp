39bab00be1944ca9a505564607ad9df7888abe77
Karol Zmijewski <karol.zmijewski@perpetual-intelligence.com>
2025-10-05 21:09:36 +0200

    [FPGA] Changed directories structure to match better to simics simulation directory tree

--- hw/hls/riscv-ips/fde_ip/fde_decode.hpp
+++ hw/hls/riscv-ips/fde_ip/fde_decode.hpp
@@ -28,7 +28,7 @@
  * source registers (rs1, rs2), function codes (func3, func7),
  * and determines the instruction type. It also decodes the immediate value
  * based on the instruction type.
- * M/O - Mandatory/Optional, In/Out - Input/Output. 
+ * M/O - Mandatory/Optional, In/Out - Input/Output.
  * @param instr [M][In] The instruction to be decoded.
  * @param p_dec_instr [M][Out] Pointer to store the decoded instruction components.
  */
